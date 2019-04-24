d34fae6f1e288ba0ed8f9f21f481a1b0e8b18f9e
JGrytting <Jonnygrytting90@gmail.com>
2019-04-24 17:00:56 +0200

    Second commit

--- SmallProjects/src/FibonacciBad.java
+++ SmallProjects/src/FibonacciBad.java
@@ -1,10 +1,9 @@
-// TEST
+// TEST 2
 public class FibonacciBad {
 	// I was too lazy/stupid too create a main class so I call this class from GuessingGame...
 	
 	// Recursive way to calculate Fibonacci numbers. It's bad. Integer overflow at 57th number.
 	public int fibo(int i) {
-			int test = 2;
 			if (i == 0) return 0;
 			else if (i == 1) return 1;
 			else return fibo(i-1) + fibo(i-2);
